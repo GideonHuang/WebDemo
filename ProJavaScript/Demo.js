9c1b716b0a1bd43ae6165468eaf2418cbc77790d
GideonHuang <csjmhuang@HuangmatoAir.lan>
2015-05-31 19:43:45 +0800

    addDemoCode

--- ProJavaScript/Demo.js
+++ ProJavaScript/Demo.js
@@ -1,3 +1,4 @@
+//program section for demo1()
 function displayInfo(args)
 {
 	var output = "";
@@ -13,7 +14,8 @@ function displayInfo(args)
 	return output;
 }
 
-function demo1() {
+function demo1() 
+{
 	alert(displayInfo({
 		name: "Huangjueming",
 		age: 32
@@ -24,3 +26,62 @@ function demo1() {
 		}));
 }
 
+//program section for demo2()
+function demo2()
+{
+	alert(callSomeFunction(add20, 5));
+
+	alert(callSomeFunction(sayHello, "Ma Tianying"));
+}
+
+function callSomeFunction(funcName, funcArg)
+{
+	return funcName(funcArg);
+}
+
+function add20(baseNum)
+{
+	return baseNum + 20;
+}
+
+function sayHello(guestName)
+{
+	return "Good Morning, " + guestName;
+}
+
+//program section for demo3()
+function demo3()
+{
+	var data = [{name: "Huangjueming", age: 32}, {name: "Matianying", age: 28}];
+
+	data.sort(createComparisonFunction("age"));
+	var firstNameSortByAge = data[0].name;
+	alert(firstNameSortByAge);
+
+	data.sort(createComparisonFunction("name"));
+	var firstNameSortByName = data[0].name;
+	alert(firstNameSortByName);
+}
+
+function createComparisonFunction(propertyName) {
+	return function(obj1, obj2) {
+		return obj1[propertyName] - obj2[propertyName];
+	}
+}
+
+//program section for demo4()
+function demo4()
+{
+	window.color = "red";
+
+	var obj = {color: "blue"};
+
+	function sayColor()
+	{
+		alert(this.color);
+	}
+
+	sayColor.call(this);
+	sayColor.call(window);
+	sayColor.call(obj);
+}
